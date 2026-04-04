134f40e24e19f54970f82ade4a23cbf86073a7bf
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-04-05 00:30:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.7.0'
-        classpath 'com.google.gms:google-services:4.4.2'
+        classpath("com.android.tools.build:gradle:8.7.0")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
 }
 
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
