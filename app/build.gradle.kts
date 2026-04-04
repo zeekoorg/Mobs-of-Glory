3b98278aa19f55d49aac787bc3c2c2fad0aafdf3
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-04-05 00:43:28 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0") // نسخة حديثة متوافقة مع SDK 35
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
