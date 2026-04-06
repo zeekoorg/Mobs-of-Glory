86b0fc40055735d834c152377b65f4f9c5d6b4c6
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-04-06 20:17:55 +0300

    Update GameActivity.kt

--- app/src/main/java/com/zeekoorg/mobsofglory/GameActivity.kt
+++ app/src/main/java/com/zeekoorg/mobsofglory/GameActivity.kt
@@ -4,9 +4,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var gameEngine: GameEngine
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // ربط الشاشة بواجهة ساحة المعركة
-        setContentView(R.layout.activity_game)
+        
+        // هنا السحر: بدلاً من استخدام setContentView(R.layout...)
+        // نقوم بجعل المحرك الذي برمجناه هو الشاشة بالكامل!
+        gameEngine = GameEngine(this)
+        setContentView(gameEngine)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameEngine.resume() // تشغيل حلقة اللعبة
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gameEngine.pause() // إيقاف اللعبة مؤقتاً لتوفير البطارية
     }
 }
