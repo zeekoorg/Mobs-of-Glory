a405751b90e507c59a71bd8f5e81175440735cd9
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-04-07 04:01:18 +0300

    Update GameActivity.kt

--- app/src/main/java/com/zeekoorg/mobsofglory/GameActivity.kt
+++ app/src/main/java/com/zeekoorg/mobsofglory/GameActivity.kt
@@ -1,10 +1,6 @@
 package com.zeekoorg.mobsofglory
 
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
@@ -14,20 +10,8 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // إخفاء شريط الحالة (Status Bar) وشريط الأزرار السفلي لتكون الشاشة كاملة
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.let {
-                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            @Suppress("DEPRECATION")
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
-
+        // قمنا بإزالة كود الشاشة الكاملة المعقد مؤقتاً
+        // هنا نقوم فقط بإنشاء المحرك وعرضه كشاشة أساسية
         gameEngine = GameEngine(this)
         setContentView(gameEngine)
     }
