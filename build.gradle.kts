e75c4f7841b4992bcbddfb1d14ea412843ce4c47
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-04-09 02:19:04 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,18 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        // مستودع هواوي الأساسي لجلب البلوقن
+        maven { url = uri("https://developer.huawei.com/repo/") }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.0")
-        classpath("com.google.gms:google-services:4.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        
+        // بلوقن خدمات جوجل
+        classpath("com.google.gms:google-services:4.4.2")
+        
+        // بلوقن خدمات هواوي (أحدث إصدار مستقر)
+        classpath("com.huawei.agconnect:agcp:1.9.1.302")
     }
 }
 
