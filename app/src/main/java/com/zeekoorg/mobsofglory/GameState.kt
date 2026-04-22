bddb98096b09b1fdfd40aaa82060b2ed802222ed
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-04-23 02:18:38 +0300

    Update GameState.kt

--- app/src/main/java/com/zeekoorg/mobsofglory/GameState.kt
+++ app/src/main/java/com/zeekoorg/mobsofglory/GameState.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import java.util.Locale
 import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.math.pow
 import kotlin.random.Random
